17ae48c92e750862c3c7731f2c7e659639d6ec1a
zad-ireda <150346573+zad-ireda@users.noreply.github.com>
2023-11-16 05:53:17 -0800

    Update res_user_inherit.py

--- second_zad_task/models/res_user_inherit.py
+++ second_zad_task/models/res_user_inherit.py
@@ -4,6 +4,6 @@ from odoo import fields, models
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    res_user_phone = fields.Char(related='partner_id.phone', string='phone'readonly=True)
+    res_user_phone = fields.Char(related='partner_id.phone', string='phone',readonly=True)
     
-    res_user_mobile =  fields.Char(related='partner_id.mobile', string='mobile' readonly=True)
+    res_user_mobile =  fields.Char(related='partner_id.mobile', string='mobile' ,readonly=True)
